8c237d7ed0b6030a2245bb2e1410eb4930622c3b
Alien089 <gabri.moratelli@gmail.com>
2024-02-26 23:57:30 +0100

    working on snapping

--- Assets/Scripts/DoorChecker.cs
+++ Assets/Scripts/DoorChecker.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Overlays;
+using UnityEditor;
 using UnityEngine;
+using UnityEditor.PackageManager;
 
 [ExecuteInEditMode]
 public class DoorChecker : MonoBehaviour
 {
+    private bool m_Lock = false;
+    private Vector3 m_LockPosition;
     void Update()
     {
-        // Ottieni la posizione e la direzione del raycast
-        Ray ray = new Ray(transform.position, transform.forward);
-
-        // Esegui il raycast
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 3f))
+        if (m_Lock && !Application.isPlaying)
         {
-            if (hit.collider.gameObject.tag == "Door")
+            gameObject.transform.parent.position = m_LockPosition;
+            EditorUtility.SetDirty(transform.parent);
+        }
+        else
+        {
+            RaycastHit hit;
+            if(Physics.BoxCast(transform.position, new Vector3(0.5f, 2, 0.2f), Vector3.forward, out hit, Quaternion.identity, 3f))
             {
-                gameObject.transform.parent.transform.position = hit.collider.transform.position;
+                if(hit.collider.gameObject.tag == "Door")
+                {
+                    m_Lock = true;
+                    m_LockPosition = gameObject.transform.parent.position;
+                }
             }
         }
     }
